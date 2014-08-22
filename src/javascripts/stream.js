4f7d96d74c8fc5bd2d5bf2955e584c617d4b807e
Roland Vachter <roland.vachter@ft.com>
2014-08-22 11:34:52 +0300

    Reading config bug in stream.js fixed.

--- src/javascripts/stream.js
+++ src/javascripts/stream.js
@@ -4,7 +4,7 @@ var envConfig = require('./config.js');
 exports.init = function (collectionId, lastEventId) {
     "use strict";
 
-    var lfStreamUrl = "https://"+ envConfig.livefyre.network +".stream1.fyre.co/v3.0/collection/"+ collectionId +"/"+ lastEventId +"/";
+    var lfStreamUrl = "https://"+ envConfig.get().livefyre.network +".stream1.fyre.co/v3.0/collection/"+ collectionId +"/"+ lastEventId +"/";
 
     request.get(lfStreamUrl);
 };
